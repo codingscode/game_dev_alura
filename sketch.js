8faa676967384d6d78f931a3bf5ba0102d6c3118
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-28 12:37:31 -0300

    parte 8

--- sketch.js
+++ sketch.js
@@ -13,12 +13,25 @@ function setup() {
 }
 
 function draw() {
-    image(imagemCenario, -50, 0, width, height)
-    image(imagemCenario, width-50, 0, width, height)
     image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270) // (+) 1ª figurinha, dimensoes alt e larg
     
-
 }
+
+class Cenario {
+    constructor(imagem, velocidade) {
+        this.imagem = imagem
+        this.velocidade = velocidade
+    }
+
+    exibe() {
+        image(this.imagemCenario, -50, 0, width, height)
+        image(this.imagemCenario, width-50, 0, width, height)
+      
+    }
+}
+
+
+
 
 
 
