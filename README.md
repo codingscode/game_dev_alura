dfd246770a90b0a18430f1e6a22199ac838ac7bf
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-28 11:34:41 -0300

    parte 3

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### Jogo criado pela dev alura
 
+Instalar extensão live server no vscode e abrir arquivo no vscode com o botao direito em 'open with live server'
+tips:
+    https://www.youtube.com/watch?v=l6DWUE5vZVU
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,13 +1,26 @@
+
+let imagemCenario
+let imagemPersonagem
+
+function preload() {
+  imagemCenario = loadImage('./imagens/cenario/floresta.png')
+  imagemPersonagem = loadImage('./imagens/personagem/correndo.png')
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    
 }
 
 function draw() {
-    background(220)
-    circle(mouseX, mouseY, 100)
+    background(imagemCenario)
+    image(imagemPersonagem, 0, 0)
+    /* circle(mouseX, mouseY, 100) */
     /* console.log('algo infinito...') */
-    
-
 
 
 }
+
+
+
+
