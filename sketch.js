6431fe3204bd3a772e03605bada07548566e38c7
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-29 12:40:08 -0300

    parte 16

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight)
     cenario = new Cenario(imagemCenario, 2.5)
-    somDoJogo.play()
+    somDoJogo.loop() // quando a musica termina repete
 }
 
 function draw() {
@@ -24,34 +24,7 @@ function draw() {
     
 }
 
-class Cenario {
-    constructor(imagem, velocidade) {
-        this.imagem = imagem
-        this.velocidade = velocidade
-        this.x1 = 0
-        this.x2 = width
-    }
 
-    exibe() {
-        image(this.imagem, this.x1 -this.velocidade, 0, width, height)
-        image(this.imagem, this.x2 + 1, 0, width, height)
-      
-    }
-
-    move() {
-        this.x1 -= this.velocidade
-        this.x2 -= this.velocidade
-    
-        if (this.x1 < -width) {
-          this.x1 = width
-        }
-
-        if (this.x2 < -width) {
-          this.x2 = width
-        }
-    }
-    
-}
 
 
 
