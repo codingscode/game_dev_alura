5da3f1b8f381fb936dd3865edbfc98399f609257
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-29 13:14:02 -0300

    parte 19

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,9 +1,19 @@
 class Personagem {
     constructor(imagem) {
        this.imagem = imagem
+       this.matriz = [
+            [0, 0],   [220, 0],   [440, 0],   [660, 0],
+            [0, 270], [220, 270], [440, 270], [660, 270],
+            [0, 540], [220, 540], [440, 540], [660, 540], 
+            [0, 810], [220, 810], [440, 810], [660, 810]
+      ]
     }
     
     exibe() {
-        image(this.imagem, 0, height - 135, 110, 135, 0, 0, 220, 270)  
+        image(this.imagem, 0, height - 135, 110, 135, this.matriz[0][0], this.matriz[0][1], 220, 270)  
+    }
+
+    anima() {
+
     }
 }
