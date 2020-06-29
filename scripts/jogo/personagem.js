3df6ba40786749bd3fa49ee8497cb4be2bae16d9
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-29 13:20:20 -0300

    parte 20

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -6,14 +6,16 @@ class Personagem {
             [0, 270], [220, 270], [440, 270], [660, 270],
             [0, 540], [220, 540], [440, 540], [660, 540], 
             [0, 810], [220, 810], [440, 810], [660, 810]
-      ]
+       ]
+       this.frameAtual= 0
     }
     
     exibe() {
-        image(this.imagem, 0, height - 135, 110, 135, this.matriz[0][0], this.matriz[0][1], 220, 270)  
+        image(this.imagem, 0, height - 135, 110, 135, this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270)  
+        this.anima()
     }
 
     anima() {
-
+        this.frameAtual++
     }
 }
