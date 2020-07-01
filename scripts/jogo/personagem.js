ff8802a71975bdb540f7de9889006323e575dab8
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-01 11:30:04 -0300

    parte 30

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -5,16 +5,5 @@ class Personagem extends Animacao {
        this.frameAtual= 0
     }
 
-    exibe() {
-        image(this.imagem, 0, height - 135, 110, 135, this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270)  
-        this.anima()
-    }
-        
-    /* anima() {
-        this.frameAtual++
-
-        if (this.frameAtual >= this.matriz.length - 1) {
-            this.frameAtual = 0
-        }
-    } */
+    
 }
