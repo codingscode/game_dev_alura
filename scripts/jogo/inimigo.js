5d1a766ec058b15ee1ef53c5603b3019f3dcf394
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-02 11:20:26 -0300

    parte 45

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,14 +1,15 @@
 class Inimigo extends Animacao {
-    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade, atraso) {
         super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite)
           
-        this.velocidade = 8
+        this.velocidade = velocidade
+        this.atraso = atraso
     }
 
     move() {
         this.x -= this.velocidade
 
-        if (this.x < -this.largura) {
+        if (this.x < -this.largura - this.atraso) {
             this.x = width
         }
     }
