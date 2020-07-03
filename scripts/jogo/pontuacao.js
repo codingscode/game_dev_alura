86909283493fdc2d736c44b68eac8964a1ed60e0
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-03 10:44:19 -0300

    parte 53

--- scripts/jogo/pontuacao.js
+++ scripts/jogo/pontuacao.js
@@ -7,11 +7,11 @@ class Pontuacao {
        textAlign(RIGHT)
        fill('white')
        textSize(50)
-       text(this.pontos, width - 30, 50)
+       text(parseInt(this.pontos), width - 30, 50)
 
     }
 
     adicionarPonto() {
-       this.pontos += 0.2
+       this.pontos += 0.1
     }
 }
