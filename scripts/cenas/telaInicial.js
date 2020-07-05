ec6750f23234ab515567a462df03266b13834020
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-05 12:40:39 -0300

    parte 72

--- scripts/cenas/telaInicial.js
+++ scripts/cenas/telaInicial.js
@@ -4,7 +4,18 @@ class TelaInicial {
     }
 
     draw() {
-       //console.log('tela inicial')
-       image(imagemTelaInicial, 0, 0, width, height)
+       this._imagemDeFundo()
+       this._texto()
+    }
+
+    _imagemDeFundo() {
+        image(imagemTelaInicial, 0, 0, width, height)
+    }
+
+    _texto() {
+        textSize(46)
+        textFont(fonteTelaInicial)
+        text('As aventuras de', width/3 + 50, 90)
+        text('Hipsta', width/3 + 120, 130)
     }
 }
