c97b4d92006144ce73b6b8e7bffe7e4b5dc8617d
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-07 12:22:17 -0300

    parte 82

--- scripts/jogo/vida.js
+++ scripts/jogo/vida.js
@@ -2,13 +2,23 @@ class Vida {
     constructor(total, inicial) {
        this.total = total
        this.inicial = inicial
+       this.vidas = this.inicial
         
        this.largura = 50
        this.altura = 50
+       this.xInicial = 20
+       this.y = 20
     }
  
     draw() {
-        image(imagemVida, 0, 0, this.largura, this.altura)
+        for (let i=0; i<this.vidas; i++) {
+            const margem = i*10
+            const posicao = this.xInicial*(i + 1)
+
+            image(imagemVida, posicao + margem, this.y, this.largura, this.altura)
+        }
+
+        
     }
      
 }
