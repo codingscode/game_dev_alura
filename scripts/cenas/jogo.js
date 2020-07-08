5835482df69054d15d27e3ec94d96f87734ce755
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-08 00:04:21 -0300

    parte 89

--- scripts/cenas/jogo.js
+++ scripts/cenas/jogo.js
@@ -15,9 +15,9 @@ class Jogo {
         vida = new Vida(3, 3)
     
         personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270)
-        const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10, 100)
-        const inimigoGotinhaVoadora = new Inimigo(matrizGotinhaVoadora, imagemGotinhaVoadora, width - 52, 200, 100, 75, 200, 150, 10, 100)
-        const inimigoTroll = new Inimigo(matrizInimigoGrande, imagemInimigoTroll, width, 0, 200, 200, 400, 400, 10, 100)
+        const inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 30, 52, 52, 104, 104, 10)
+        const inimigoGotinhaVoadora = new Inimigo(matrizGotinhaVoadora, imagemGotinhaVoadora, width - 52, 200, 100, 75, 200, 150, 10)
+        const inimigoTroll = new Inimigo(matrizInimigoGrande, imagemInimigoTroll, width, 0, 200, 200, 400, 400, 10)
         
         inimigos.push(inimigo)
         inimigos.push(inimigoGotinhaVoadora)
--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,16 +1,15 @@
 class Inimigo extends Animacao {
-    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade, atraso) {
+    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade) {
         super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite)
           
         this.velocidade = velocidade
-        this.atraso = atraso
-        this.x = width + this.atraso
+        this.x = width
     }
 
     move() {
         this.x -= this.velocidade
 
-        if (this.x < -this.largura - this.atraso) {
+        if (this.x < -this.largura) {
             this.x = width
         }
     }
